68bbcb51b1851842a14c838b4549df71d888a394
w2lody <44412859+w2lody@users.noreply.github.com>
2018-11-23 22:26:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1052,4 +1052,35 @@ client.on("message", async message => {
     });
 
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+});
+const devs = ["Your ID"]; /// الاي دي حقك
+const karzoprfix = ["-"];
+client.on('message', message => {
+    var argresult = message.content.split().slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+ 
+  if (message.content.startsWith(karzoprfix + 'ply')) {/////بلاينق
+    client.user.setGame(argresult);
+      message.channel.send(:white_check_mark:   ${argresult} Done Sir)
+  } else
+  if (message.content.startsWith(karzoprfix + 'wt')) {////واتشنق
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(:white_check_mark:   ${argresult} Done Sir)
+  } else
+  if (message.content.startsWith(karzoprfix + 'st')) {////ستريمنق
+    client.user.setGame(argresult, "https://www.twitch.tv/idk%22);
+      message.channel.send(:white_check_mark:)
+  } else
+  if (message.content.startsWith(karzoprfix + 'ls')) {/////////ليسننق
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(:white_check_mark:   ${argresult} Done Sir)
+client.user.setStatus("dnd")
+  }
+});
+
+
 client.login(process.env.BOT_TOKEN);
